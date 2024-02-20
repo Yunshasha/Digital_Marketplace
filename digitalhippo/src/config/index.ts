126b4e9913431dd51d1af156b6da9c750a34010d
Yunshasha <syun@red.software.systems>
2024-02-20 15:35:05 +0100

    update navbar items

--- digitalhippo/src/config/index.ts
+++ digitalhippo/src/config/index.ts
@@ -8,6 +8,37 @@ export const PRODUCT_CATEGORIES = [
         href: "#",
         imageSrc: "/nav/ui_kits/mixed.jpg",
       },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/ui_kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/ui_kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    feature: [
+      {
+        name: "Favorite Icons Picks",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
     ],
   },
 ];
