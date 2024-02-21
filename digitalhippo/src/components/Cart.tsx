363e2c02ecfb38a0c4208038552d53fbaccc4f69
Yunshasha <syun@red.software.systems>
2024-02-21 16:38:24 +0100

    update cart sidebar footer

--- digitalhippo/src/components/Cart.tsx
+++ digitalhippo/src/components/Cart.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
 import { ShoppingCart } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 
 function Cart() {
+  const itemCount = 1;
+  const fee = 1;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -25,6 +32,43 @@ function Cart() {
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart(0)</SheetTitle>
         </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className=" flex w-full flex-col pr-6">
+              cart items
+              {/* todo : cart logic */}
+            </div>
+            <div>
+              <Separator />
+              <div className="space-y-1.5 pr-6 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span></span>
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({ className: "w-full" })}
+                  >
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
       </SheetContent>
     </Sheet>
   );
